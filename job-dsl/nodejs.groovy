d0390ffe416b714a22dc1b3bf7957ae9eb38324b
eraaaza <45674439+eraaaza@users.noreply.github.com>
2021-08-18 11:20:56 -0700

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/eraaaza/docker-demo.git', 'Master') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/eraaaza/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('eraaaza')
             node / gitConfigEmail('bperaza415@gmail.com')
         }
